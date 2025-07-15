077b3168401fee29874f8552933503f808db52a4
Max Engisch <maxengisch@gmail.com>
2025-07-15 14:58:45 +0200

    chore: Initialize the vue project with vite

---
+++ README.md
@@ -0,0 +1,30 @@
+# vue-leaflet
+
+Vue-leaflet, written and compatible with Vue 3!
+
+This branch will be used to rewrite the library to use the composition API and to migrate to leaflet v2. New components will be added soon. See the list below which components have been added already.
+
+## What works
+
+❌ LCircle \
+❌ LCircleMarker \
+❌ LControl\
+❌LControlAttribution\
+❌LControlLayers\
+❌LControlScale\
+❌LControlZoom\
+❌LFeatureGroup\
+❌LGeoJson\
+❌LIcon\
+❌LImageOverlay\
+❌LMap\
+❌LMarker\
+❌LPolygon\
+❌LPolyline\
+❌LPopup\
+❌LRectangle\
+❌LTileLayer\
+❌LTooltip\
+❌LWmsTileLayer
+
+> This library will be fully compatible with SSR.
---
+++ vite.config.ts
@@ -0,0 +1,32 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+    build: {
+        lib: {
+            entry: fileURLToPath(new URL('./src/lib.ts', import.meta.url)),
+            formats: ['es', 'cjs', 'umd'],
+            name: 'vue-leaflet',
+            fileName: (fmt) => `vue-leaflet.${fmt}.js`,
+        },
+        rollupOptions: {
+            external: ['vue', 'leaflet', /^leaflet\/.*/],
+            output: {
+                // Global variables for use in the UMD build
+                globals: {
+                    vue: 'Vue',
+                    leaflet: 'L',
+                },
+            },
+        },
+    },
+});
