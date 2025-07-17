7ce3138756d7c64d8ea5ad2f697e69691d0d9432
Max <maxengisch@gmail.com>
2025-07-17 20:51:03 +0200

    feat(playground): Update playground to use LMap component
    Implemented same structure as in the first version of vue-leaflet.

---
+++ src/playground/views/DemoHome.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { LMap } from '../../components'
+import type { MapOptions } from 'leaflet'
+
+const mapOptions: MapOptions = {
+    center: [51.505, -0.09],
+    zoom: 13
+}
+function onZoom(zoom: number) {
+    console.log('ZOOM', zoom)
+}
+</script>
+
+<template>
+    <LMap ref="map" :mapOptions="mapOptions" @update:zoom="onZoom">
+        <!--l-tile-layer
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            layer-type="base"
+            name="OpenStreetMap"
+        ></l-tile-layer-->
+    </LMap>
+</template>
+
+<style></style>
