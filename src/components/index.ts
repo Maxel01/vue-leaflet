6d7b0d67ff00f39dd836b34963bcbf136535eebe
Max <77640056+Maxel01@users.noreply.github.com>
2025-07-22 20:22:46 +0200

    feat(core): add LMarker component (#8)
    BREAKING CHANGE: pass layerOptions as Object instead of props (LMarker)
    
    BREAKING CHANGE: setupMarkerLayer does not return options anymore

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as LMap } from "./LMap.vue";
 export { default as LGridLayer } from "./LGridLayer.vue";
+export { default as LMarker } from "./LMarker.vue";
 export { default as LTileLayer } from "./LTileLayer.vue";
