6da3d99d333e6a5af88aa66634a7273f64b00b15
Max Engisch <maxengisch@gmail.com>
2025-07-23 09:39:18 +0200

    refactor(LIcon): use composables in LIcon.vue

--- src/components/LIcon.vue
+++ src/components/LIcon.vue
@@ -11,74 +11,87 @@ import { DivIcon, DomEvent, Icon } from 'leaflet'
 
 const props = withDefaults(defineProps<IconProps>(), iconPropsDefaults)
 
-const root = ref<HTMLInputElement>()
-const canSetParentHtml = assertInject(CanSetParentHtmlInjection)
-const setParentHtml = assertInject(SetParentHtmlInjection)
-const setIcon = assertInject(SetIconInjection)
-
-let iconObject: Icon | DivIcon | undefined = undefined
-const attrs = useAttrs()
-
-const createIcon = (el, recreationNeeded: boolean, htmlSwapNeeded: boolean) => {
-    const elHtml = el && el.innerHTML
-    if (!recreationNeeded) {
-        if (htmlSwapNeeded && iconObject && canSetParentHtml()) {
-            setParentHtml(elHtml)
-        }
-        return
-    }
-
-    const { listeners } = remapEvents(attrs)
-    if (iconObject) {
-        DomEvent.off(iconObject, listeners)
-    }
-    let optionsHtml = undefined
-    if (elHtml) {
-        optionsHtml = elHtml
-    }
-
-    iconObject = optionsHtml ? new DivIcon(props.options) : new Icon(props.options)
-    DomEvent.on(iconObject, listeners)
-    setIcon(iconObject)
-}
-
-const scheduleCreateIcon = () => {
-    nextTick(() => createIcon(root.value, true, false))
-}
-
-const scheduleHtmlSwap = () => {
-    nextTick(() => createIcon(root.value, false, true))
-}
-
-const methods = {
-    setIconUrl: scheduleCreateIcon,
-    setIconRetinaUrl: scheduleCreateIcon,
-    setIconSize: scheduleCreateIcon,
-    setIconAnchor: scheduleCreateIcon,
-    setPopupAnchor: scheduleCreateIcon,
-    setTooltipAnchor: scheduleCreateIcon,
-    setShadowUrl: scheduleCreateIcon,
-    setShadowRetinaUrl: scheduleCreateIcon,
-    setShadowAnchor: scheduleCreateIcon,
-    setBgPos: scheduleCreateIcon,
-    setClassName: scheduleCreateIcon,
-    setHtml: scheduleCreateIcon,
-}
-
-onMounted(async () => {
-    propsBinder(methods, {}, props)
-
-    const observer = new MutationObserver(scheduleHtmlSwap)
-    observer.observe(root.value!, {
-        attributes: true,
-        childList: true,
-        characterData: true,
-        subtree: true,
-    })
-    scheduleCreateIcon()
-})
-
+const {root} = useIcon()
 defineExpose({ root })
+
+function useIcon() {
+    const root = ref<HTMLElement>()
+
+    const {scheduleCreateIcon, scheduleHtmlSwap} = useCreateIcon()
+    const methods = {
+        setIconUrl: scheduleCreateIcon,
+        setIconRetinaUrl: scheduleCreateIcon,
+        setIconSize: scheduleCreateIcon,
+        setIconAnchor: scheduleCreateIcon,
+        setPopupAnchor: scheduleCreateIcon,
+        setTooltipAnchor: scheduleCreateIcon,
+        setShadowUrl: scheduleCreateIcon,
+        setShadowRetinaUrl: scheduleCreateIcon,
+        setShadowAnchor: scheduleCreateIcon,
+        setBgPos: scheduleCreateIcon,
+        setClassName: scheduleCreateIcon,
+        setHtml: scheduleCreateIcon,
+    }
+
+    onMounted(async () => {
+        propsBinder(methods, {}, props)
+
+        const observer = new MutationObserver(scheduleHtmlSwap)
+        observer.observe(root.value!, {
+            attributes: true,
+            childList: true,
+            characterData: true,
+            subtree: true,
+        })
+        scheduleCreateIcon()
+    })
+    return {root}
+}
+
+function useCreateIcon() {
+    const canSetParentHtml = assertInject(CanSetParentHtmlInjection)
+    const setParentHtml = assertInject(SetParentHtmlInjection)
+    const setIcon = assertInject(SetIconInjection)
+    const attrs = useAttrs()
+
+    let iconObject: Icon | DivIcon | undefined = undefined
+
+    const createIcon = (el: HTMLElement, recreationNeeded: boolean, htmlSwapNeeded: boolean) => {
+        const elHtml = el && el.innerHTML
+        if (!recreationNeeded) {
+            if (htmlSwapNeeded && iconObject && canSetParentHtml()) {
+                setParentHtml(elHtml)
+            }
+            return
+        }
+
+        const {listeners} = remapEvents(attrs)
+        if (iconObject) {
+            DomEvent.off(iconObject, listeners)
+        }
+        const options = props.iconOptions || {}
+        if (elHtml) {
+            options.html = elHtml
+        }
+
+        iconObject = options.html ? new DivIcon(options) : new Icon(options)
+        DomEvent.on(iconObject, listeners)
+        setIcon(iconObject)
+    }
+
+    const scheduleCreateIcon = () => {
+        nextTick(() => {
+            if (root.value) createIcon(root.value, true, false)
+        })
+    }
+
+    const scheduleHtmlSwap = () => {
+        nextTick(() => {
+            if (root.value) createIcon(root.value, false, true)
+        })
+    }
+    return {scheduleCreateIcon, scheduleHtmlSwap}
+}
 </script>
 
 <template>
--- src/functions/icon.ts
+++ src/functions/icon.ts
@@ -1,9 +1,9 @@
-import type { BaseIconOptions, PointExpression } from 'leaflet'
+import type { DivIconOptions } from 'leaflet'
 import { type ComponentProps, componentPropsDefaults } from './component.ts'
 
+// BREAKING CHANGES: pass iconOptions as Object instead of props
 export interface IconProps extends ComponentProps {
-    iconOptions?: BaseIconOptions
-    bgPos?: PointExpression
+    iconOptions?: DivIconOptions
 }
 
 export const iconPropsDefaults = {
