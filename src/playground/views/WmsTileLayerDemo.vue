e1e23d51818a4b7dfd8bbf3afc9e1baa5132cc47
Max Engisch <maxengisch@gmail.com>
2025-07-24 20:46:44 +0200

    fix(LWmsTileLayer): change example url

--- src/playground/views/WmsTileLayerDemo.vue
+++ src/playground/views/WmsTileLayerDemo.vue
@@ -8,15 +8,15 @@ const zoom = ref<number>(2)
 <template>
     <LMap ref="map" v-model:zoom="zoom" :center="[47.41322, -1.219482]">
         <LWmsTileLayer
-            url="https://maps.heigit.org/osm-wms/service"
-            attribution="HeiGIT <a href='osm-wms.de'>OSM WMS</a>"
+            url="https://ows.mundialis.de/services/service?"
+            attribution="Mundialis"
             layer-type="base"
-            name="osm-wms.de"
+            name="mundialis.de"
             :max-zoom="10"
             version="1.3.0"
             format="image/png"
             :transparent="true"
-            layers="osm_auto:all"
+            :layers="'TOPO-WMS,OSM-Overlay-WMS'"
         />
     </LMap>
 </template>
