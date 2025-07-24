e80da943f7c1aee7c5a514155d62dce17a828423
Max Engisch <maxengisch@gmail.com>
2025-07-24 16:53:51 +0200

    feat(LMarker): revert breaking changes (pass layerOptions as Object instead of props (LMarker)) and (setupMarkerLayer does not return options anymore)

--- src/playground/views/MarkerDemo.vue
+++ src/playground/views/MarkerDemo.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 import { LMap, LMarker, LTileLayer } from '../../components'
-import type { LatLngTuple, MarkerOptions } from 'leaflet'
+import type { LatLngTuple } from 'leaflet'
 import { ref } from 'vue'
 
 const zoom = ref<number>(2)
 const coordinates: LatLngTuple = [50, 50]
-const markerOptions: MarkerOptions = {
-    draggable: true,
-}
 </script>
 
 <template>
@@ -18,7 +15,7 @@ const markerOptions: MarkerOptions = {
             name="OpenStreetMap"
         ></LTileLayer>
 
-        <LMarker :lat-lng="coordinates" :layer-options="markerOptions"></LMarker>
+        <LMarker :lat-lng="coordinates" draggable></LMarker>
     </LMap>
 </template>
 
