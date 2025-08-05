6afa13607e5326738b2081d7f4a69471eb28f1d5
Max <77640056+Maxel01@users.noreply.github.com>
2025-08-05 19:55:30 +0200

    test: add tests for `utils.ts` and basic tests for `LControlAttribution` (#34)

---
+++ tests/unit/utils/bindEventHandlers.test.ts
@@ -0,0 +1,46 @@
+import { bindEventHandlers } from '../../../src/utils'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import type { Evented, LeafletEventHandlerFnMap } from 'leaflet'
+
+describe('bindEventHandlers', () => {
+    let mockLeafletObject: Evented
+
+    beforeEach(() => {
+        mockLeafletObject = {
+            on: vi.fn(),
+        } as unknown as Evented
+    })
+
+    it('binds all event handlers correctly', () => {
+        const handlers = {
+            click: vi.fn(),
+            mouseover: vi.fn(),
+        }
+
+        bindEventHandlers(mockLeafletObject, handlers)
+
+        expect(mockLeafletObject.on).toHaveBeenCalledTimes(2)
+        expect(mockLeafletObject.on).toHaveBeenCalledWith('click', handlers.click)
+        expect(mockLeafletObject.on).toHaveBeenCalledWith('mouseover', handlers.mouseover)
+    })
+
+    it('does not bind any events if the handler map is empty', () => {
+        const handlers = {}
+
+        bindEventHandlers(mockLeafletObject, handlers)
+
+        expect(mockLeafletObject.on).not.toHaveBeenCalled()
+    })
+
+    it('binds undefined or null handlers', () => {
+        const handlers = {
+            click: undefined,
+            zoom: null,
+        } as LeafletEventHandlerFnMap
+
+        bindEventHandlers(mockLeafletObject, handlers)
+
+        expect(mockLeafletObject.on).toHaveBeenCalledWith('click', undefined)
+        expect(mockLeafletObject.on).toHaveBeenCalledWith('zoom', null)
+    })
+})
