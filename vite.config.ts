e2e7bc7706024c1ec825ada7a147dca0f90c3ab0
Max Engisch <maxengisch@gmail.com>
2025-08-12 13:06:08 +0200

    build: add terser plugin to rollup

--- vite.config.ts
+++ vite.config.ts
@@ -3,21 +3,22 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import dts from 'vite-plugin-dts'
+import terser from '@rollup/plugin-terser'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue(), dts({tsconfigPath: './tsconfig.build.json'})],
+    plugins: [vue(), dts({ tsconfigPath: './tsconfig.build.json' })],
     resolve: {
         alias: {
-            '@': fileURLToPath(new URL('./src', import.meta.url)),
-        },
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
     },
     build: {
         lib: {
             entry: fileURLToPath(new URL('./src/lib.ts', import.meta.url)),
             formats: ['es', 'cjs', 'umd'],
             name: 'vue-leaflet',
-            fileName: (fmt) => `vue-leaflet.${fmt}.js`,
+            fileName: (fmt) => `vue-leaflet.${fmt}.js`
         },
         rollupOptions: {
             external: ['vue', 'leaflet', /^leaflet\/.*/],
@@ -25,9 +26,10 @@ export default defineConfig({
                 // Global variables for use in the UMD build
                 globals: {
                     vue: 'Vue',
-                    leaflet: 'L',
-                },
+                    leaflet: 'L'
+                }
             },
-        },
-    },
+            plugins: [terser()]
+        }
+    }
 })
