7e6b50645bedbe210669b76f328512794b1fc17b
Max Engisch <maxengisch@gmail.com>
2025-08-14 12:44:33 +0200

    tests: add remaining control tests

---
+++ tests/unit/components/LControl.test.ts
@@ -0,0 +1,46 @@
+import { flushPromises, shallowMount, type VueWrapper } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import LControl from '../../../src/components/LControl.vue'
+import { RegisterControlInjection } from '../../../src/types/injectionKeys'
+import { Control } from 'leaflet'
+import { testRemovesOnUnmount } from './helper/tests'
+import { testComponentPropBindings, testPropsBindingToLeaflet } from './helper/propsBindingTests'
+import { testEmitsReady } from './helper/emitTests'
+import { mockRegisterControl, testControlRegistration } from './helper/injectionsTests'
+
+const createWrapper = async (props = {}) => {
+    const wrapper = shallowMount(LControl, {
+        propsData: {
+            position: 'topright',
+            ...props,
+        },
+        global: {
+            provide: {
+                [RegisterControlInjection as symbol]: mockRegisterControl,
+            },
+        },
+    })
+
+    await flushPromises()
+    return wrapper
+}
+
+describe('LControl.vue', () => {
+    testEmitsReady(createWrapper)
+    testComponentPropBindings(createWrapper, "LControl")
+    testPropsBindingToLeaflet(createWrapper, { position: 'bottomleft' })
+    testRemovesOnUnmount(createWrapper)
+
+    testCorrectInitialisation(createWrapper)
+    testControlRegistration(createWrapper)
+})
+
+const testCorrectInitialisation = (getWrapper: () => Promise<VueWrapper<any>>) => {
+    it('creates a Leaflet Attribution control with correct options', async () => {
+        const wrapper = await getWrapper()
+        const obj = wrapper.vm.leafletObject as Control
+
+        expect(obj).toBeDefined()
+        expect(obj.options.position).toBe('topright')
+    })
+}
---
+++ tests/unit/components/LControlZoom.test.ts
@@ -0,0 +1,46 @@
+import { flushPromises, shallowMount, type VueWrapper } from '@vue/test-utils'
+import { describe, expect, it } from 'vitest'
+import LControlZoom from '../../../src/components/LControlZoom.vue'
+import { RegisterControlInjection } from '../../../src/types/injectionKeys'
+import { Control } from 'leaflet'
+import { testRemovesOnUnmount } from './helper/tests'
+import { testComponentPropBindings, testPropsBindingToLeaflet } from './helper/propsBindingTests'
+import { testEmitsReady } from './helper/emitTests'
+import { mockRegisterControl, testControlRegistration } from './helper/injectionsTests'
+
+const createWrapper = async (props = {}) => {
+    const wrapper = shallowMount(LControlZoom, {
+        propsData: {
+            position: 'topright',
+            ...props,
+        },
+        global: {
+            provide: {
+                [RegisterControlInjection as symbol]: mockRegisterControl,
+            },
+        },
+    })
+
+    await flushPromises()
+    return wrapper
+}
+
+describe('LControlZoom.vue', () => {
+    testEmitsReady(createWrapper)
+    testComponentPropBindings(createWrapper, "LControlZoom")
+    testPropsBindingToLeaflet(createWrapper, { position: 'bottomleft' })
+    testRemovesOnUnmount(createWrapper)
+
+    testCorrectInitialisation(createWrapper)
+    testControlRegistration(createWrapper)
+})
+
+const testCorrectInitialisation = (getWrapper: () => Promise<VueWrapper<any>>) => {
+    it('creates a Leaflet Attribution control with correct options', async () => {
+        const wrapper = await getWrapper()
+        const obj = wrapper.vm.leafletObject as Control.Zoom
+
+        expect(obj).toBeDefined()
+        expect(obj.options.position).toBe('topright')
+    })
+}
