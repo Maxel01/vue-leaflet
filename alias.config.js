15c391060eb9bffde8ce39c63b586d07f4d22304
Max Engisch <maxengisch@gmail.com>
2025-08-16 12:04:49 +0200

    chore: add alias and remove obsolete alias

--- alias.config.js
+++ alias.config.js
@@ -1,7 +1,6 @@
 import { fileURLToPath, URL } from 'node:url'
 
 export const alias = {
-    '@/maxel01/vue-leaflet': fileURLToPath(new URL('./src/lib', import.meta.url)),
     '@/playground': fileURLToPath(new URL('./playground', import.meta.url)),
     '@': fileURLToPath(new URL('./src', import.meta.url))
 }
