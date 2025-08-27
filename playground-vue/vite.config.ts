8b2b131491b6ca267b1ae23d627f27bc7a6dbc68
Max Engisch <maxengisch@gmail.com>
2025-08-27 23:09:48 +0200

    fix(playground-vue): add missing aliases to resolve modules

--- playground-vue/vite.config.ts
+++ playground-vue/vite.config.ts
@@ -1,18 +1,19 @@
-import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'node:path'
+import { alias } from '../alias.config'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    plugins: [vue(), vueDevTools()],
+    resolve: {
+        alias: {
+            '@maxel01/vue-leaflet': path.resolve(__dirname, '../src/lib'),
+            '@maxel01/vue-leaflet-plugins': path.resolve(__dirname, '../plugins/src/plugins'),
+            //'@maxel01/vue-leaflet': '@maxel01/vue-leaflet'
+            //'@maxel01/vue-leaflet': '@vue-leaflet/vue-leaflet'
+            ...alias
+        }
+    }
 })
