4528ef1ba354624a1a3f0b05b65b94d6a6fc0598
Max <77640056+Maxel01@users.noreply.github.com>
2025-08-29 19:13:07 +0200

    chore(build): improve build and playground deps (#60)

--- playground-vue/vite.config.ts
+++ playground-vue/vite.config.ts
@@ -4,14 +4,19 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import path from 'node:path'
 import { alias } from '../alias.config'
 
+const version: 'local' | 'dist' | 'npm' = 'dist'
+const vue_leaflet = {
+    local: path.resolve(__dirname, '../src/lib'),
+    dist: '@dist/vue-leaflet',
+    npm: '@maxel01/vue-leaflet'
+}
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue(), vueDevTools()],
     resolve: {
         alias: {
-            '@maxel01/vue-leaflet': path.resolve(__dirname, '../src/lib'),
-            //'@maxel01/vue-leaflet': '@maxel01/vue-leaflet'
-            //'@maxel01/vue-leaflet': '@vue-leaflet/vue-leaflet'
+            '@maxel01/vue-leaflet': vue_leaflet[version],
             ...alias
         }
     }
