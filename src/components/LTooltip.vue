dce903053f45f9a1030ddce470ba932fcef27d96
Max Engisch <maxengisch@gmail.com>
2025-08-31 12:10:48 +0200

    chore: format files

--- src/components/LTooltip.vue
+++ src/components/LTooltip.vue
@@ -1,10 +1,9 @@
 <script setup lang="ts">
 import { Tooltip } from 'leaflet'
 import { markRaw, nextTick, onMounted, ref, useAttrs } from 'vue'
-import { setupTooltip } from '@/functions/tooltip'
+import { setupTooltip, type TooltipProps, tooltipPropsDefaults } from '@/functions/tooltip'
 import { BindTooltipInjection } from '@/types/injectionKeys'
 import { assertInject, propsBinder, remapEvents } from '@/utils'
-import { type TooltipProps, tooltipPropsDefaults } from '@/functions/tooltip'
 
 /**
  * > Display a tooltip on the map
