8cd29ae247890ae97eef5b9c2eea11630464164b
Max Engisch <maxengisch@gmail.com>
2025-09-02 21:49:02 +0200

    chore(tests): move markerProps to wrapper file

--- tests/unit/components/LMarker.test.ts
+++ tests/unit/components/LMarker.test.ts
@@ -1,29 +1,14 @@
 import { type VueWrapper } from '@vue/test-utils'
 import { describe, expect, it } from 'vitest'
-import { Circle, LatLng, Marker } from 'leaflet'
+import { Circle, LatLng } from 'leaflet'
 import { testRemoveLayerOnUnmount } from '@/tests/helper/tests'
 import {
-    layerProps,
     testComponentPropBindings,
     testPropsBindingToLeaflet
 } from '@/tests/helper/propsBindingTests'
 import { testEmitsReady } from '@/tests/helper/emitTests'
 import { testAddLayer } from '@/tests/helper/injectionsTests'
-import { mergeReactiveProps } from '@/tests/helper/props'
-import { createMarkerWrapper } from './wrapper/LMarker'
-
-const markerProps = mergeReactiveProps(layerProps, {
-    // TEST draggable: true,
-    // TEST icon: ?,
-    zIndexOffset: 5,
-    latLng: new LatLng(44.5, 11.5),
-    expecting: {
-        draggable(leafletObject: Marker) {
-            // TEST leafletObject.dragging is undefined. Why?
-            expect(leafletObject.dragging.enabled()).toBeTruthy()
-        }
-    }
-})
+import { createMarkerWrapper, markerProps } from './wrapper/LMarker'
 
 describe('LMarker.vue', () => {
     testEmitsReady(createMarkerWrapper)
